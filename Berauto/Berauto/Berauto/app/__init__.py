17986579d2d032d11c448523e6e677410f442d8b
peterbarbara0608 <peterbarbara0608@gmail.com>
2025-03-23 20:26:50 +0100

    Végleges verzió 2.

--- Berauto/Berauto/Berauto/app/__init__.py
+++ Berauto/Berauto/Berauto/app/__init__.py
@@ -1,5 +1,5 @@
 #from turtle import title
-from apiflask import APIFlask
+from flask import Flask
 from config import Config
 from app.extensions import db
 
@@ -9,18 +9,18 @@ from flask_migrate import Migrate
 
 
 def create_app(config_class=Config):
-    #app = Flask(__name__)
-    app=APIFlask(__name__, json_errors = True, title="Berauto", docs_path="/swagger")
+    app = Flask(__name__)
+    #app=APIFlask(__name__, json_errors = True, title="Berauto", docs_path="/swagger")
     
     app.config.from_object(config_class)
 
     # Initialize Flask extensions here
     db.init_app(app)
     from flask_migrate import Migrate
-    migrate = Migrate(app, db, render_as_batch=True)
+    migrate = Migrate(app, db)
 
     # Register blueprints here
     from app.main import bp as main_bp
-    app.register_blueprint(main_bp, url_prefix="/api")
+    app.register_blueprint(main_bp)
 
     return app

266b2bbfab6f4101eb558d5a2dd11524800e0467
peterbarbara0608 <peterbarbara0608@gmail.com>
2025-03-23 20:22:14 +0100

    Végleges verzió.

--- Berauto/Berauto/Berauto/app/__init__.py
+++ Berauto/Berauto/Berauto/app/__init__.py
@@ -1,18 +1,26 @@
-from flask import Flask
+#from turtle import title
+from apiflask import APIFlask
 from config import Config
 from app.extensions import db
 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from flask_migrate import Migrate
+
+
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    #app = Flask(__name__)
+    app=APIFlask(__name__, json_errors = True, title="Berauto", docs_path="/swagger")
+    
     app.config.from_object(config_class)
 
     # Initialize Flask extensions here
     db.init_app(app)
     from flask_migrate import Migrate
-    migrate = Migrate(app, db)
+    migrate = Migrate(app, db, render_as_batch=True)
 
     # Register blueprints here
     from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
+    app.register_blueprint(main_bp, url_prefix="/api")
 
     return app
--- Berauto/Berauto/Berauto/init_db.py
+++ Berauto/Berauto/Berauto/init_db.py
@@ -4,17 +4,8 @@ from app import db
 from app import create_app
 from config import Config
 
-app=create_app(config_class=Config)
+app = create_app(config_class=Config)
+
 app.app_context().push()
-
-
-
-
-from app.models.role import Role
-from app.models.address import Address
-from app.models.user import User
-from app.models.rental import Rental
-from app.models.car import Car
-
 
 
